c58f894ff89e383f9edea315081736da663f5612
Julian Kaindl <kaindl745@gmail.com>
2020-07-13 13:52:13 +0200

    Add markdown generating to benchmarks

--- benchmark/benchmark.config.ts
+++ benchmark/benchmark.config.ts
@@ -10,4 +10,7 @@ for (let i = 0; i < EVICT; i++) {
   DATA_2[i] = [i.toString(), Math.floor(Math.random() * 1e7)];
 }
 
-export { MAX_KEYS, EVICT, RUNS, DATA_1, DATA_2 };
+const CACHES = ['LRU', 'RR'];
+const MARKDOWN_OUT = './benchmark/results.md';
+
+export { MAX_KEYS, EVICT, RUNS, DATA_1, DATA_2, CACHES, MARKDOWN_OUT };
